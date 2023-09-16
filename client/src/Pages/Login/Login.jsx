a8970bb833494d0262659d7732a9064dbaab907b
Nikita Birajdar <niksbirajdar12@gmail.com>
2023-09-16 16:10:23 +0530

    Login page updated

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './Login.css'
+import Logo from '../../Assets/Main_Logo.png'
+import eyeLogo from '../../Assets/eye.svg'
+import { useState } from 'react'
 
 export default function Login() {
+    const [eye, setEye] = useState(false);
   return (
     <div className="body">
         <div className="frame6-container">
@@ -9,15 +13,16 @@ export default function Login() {
           <div className="frame6-social"></div>
           <div className="frame6-frame4">
             <img
-              src="public/external/image180320-cd3e1-200h.png"
-              alt="image180320"
+              src={Logo}
+              alt="Logo-img"
               className="frame6-image18"
             />
+            
             <div className="frame6-frame7">
               <span className="frame6-text"><span>Welcome back</span></span>
               <span className="frame6-text02">
                 <span className="frame6-text03">Don’t have an account?</span>
-                <span>Sign UP</span>
+                <span> Sign UP</span>
               </span>
             </div>
           </div>
@@ -33,28 +38,30 @@ export default function Login() {
               />
             </div>
             <div className="frame6-frame8">
-              <div className="frame6-pass">
-                <span className="frame6-text07">
-                  <span className="frame6-text08">Password *</span>
-                </span>
-                <div className="frame6-input1">
-                  <span className="frame6-text09"><span>*************</span></span>
-                  <img
-                    src="public/external/vector0322-8z6n.svg"
-                    alt="Vector0322"
-                    className="frame6-vector"
-                  />
-                </div>
+              <div className="frame6-email" >
+              <span className="frame6-text05">
+                <span className="frame6-text06">Password *</span>
+              </span>
+              <div className="pwd-input" style={{display: 'flex'}}>
+                <input
+                    type={eye ? "text" : "password"}
+                    placeholder="Enter Password"
+                    className="frame6-input"
+                />
+                <img src={eyeLogo} alt="eye-btn" style={{width:20, height:20, marginLeft: 360, marginTop:40, zIndex:1}}
+                onClick={()=> setEye(!eye) }/> 
               </div>
+
+            </div>
               <div className="frame6-check">
                 <span className="frame6-text11">
                   <span>Forgotten password?</span>
                 </span>
               </div>
             </div>
-            <div className="frame6-frame1">
-              <span className="frame6-text13"><span>Log In</span></span>
-            </div>
+            <button className='frame6-frame1'>
+                <span className="frame6-text13"><span>Log In</span></span>
+            </button>
           </div>
         </div>
       </div>
