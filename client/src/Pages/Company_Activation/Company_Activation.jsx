bf4b2a918c2dd2330d160a434343fdee5fe7ebab
Nikita Birajdar <niksbirajdar12@gmail.com>
2023-10-01 12:30:06 +0530

    Update Company_Activation Page

--- client/src/Pages/Company_Activation/Company_Activation.jsx
+++ client/src/Pages/Company_Activation/Company_Activation.jsx
@@ -26,59 +26,27 @@ export default function
               </span>
               <input
                 type="text"
-                placeholder="Steven Stallion"
+                placeholder=""
                 class="activation-input"
               />
             </div>
             <div class="activation-container">
               <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
+                <span class="activation-text">Area of Work *</span>
               </span>
               <input
                 type="text"
-                placeholder="Steven Stallion"
+                placeholder=""
                 class="activation-input"
               />
             </div>
             <div class="activation-container">
               <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
+                <span class="activation-text">Work Location *</span>
               </span>
               <input
                 type="text"
-                placeholder="Steven Stallion"
-                class="activation-input"
-              />
-            </div>
-            </div>
-            <div className="activation-inner-container">
-            <div class="activation-container">
-              <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
-              </span>
-              <input
-                type="text"
-                placeholder="Steven Stallion"
-                class="activation-input"
-              />
-            </div>
-            <div class="activation-container">
-              <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
-              </span>
-              <input
-                type="text"
-                placeholder="Steven Stallion"
-                class="activation-input"
-              />
-            </div>
-            <div class="activation-container">
-              <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
-              </span>
-              <input
-                type="text"
-                placeholder="Steven Stallion"
+                placeholder=""
                 class="activation-input"
               />
             </div>
@@ -86,31 +54,63 @@ export default function
             <div className="activation-inner-container">
             <div class="activation-container">
               <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
+                <span class="activation-text">company Registration id *</span>
               </span>
               <input
                 type="text"
-                placeholder="Steven Stallion"
+                placeholder=""
                 class="activation-input"
               />
             </div>
             <div class="activation-container">
               <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
+                <span class="activation-text">Industry Type *</span>
               </span>
               <input
                 type="text"
-                placeholder="Steven Stallion"
+                placeholder=""
                 class="activation-input"
               />
             </div>
             <div class="activation-container">
               <span class="activation-container-text">
-                <span class="activation-text">Company Website *</span>
+                <span class="activation-text">Registerd office *</span>
               </span>
               <input
                 type="text"
-                placeholder="Steven Stallion"
+                placeholder=""
+                class="activation-input"
+              />
+            </div>
+            </div>
+            <div className="activation-inner-container">
+            <div class="activation-container">
+              <span class="activation-container-text">
+                <span class="activation-text">No of Employees *</span>
+              </span>
+              <input
+                type="text"
+                placeholder=""
+                class="activation-input"
+              />
+            </div>
+            <div class="activation-container">
+              <span class="activation-container-text">
+                <span class="activation-text">Service type *</span>
+              </span>
+              <input
+                type="text"
+                placeholder=""
+                class="activation-input"
+              />
+            </div>
+            <div class="activation-container">
+              <span class="activation-container-text">
+                <span class="activation-text">Payment Mode *</span>
+              </span>
+              <input
+                type="text"
+                placeholder=""
                 class="activation-input"
               />
             </div>
@@ -131,101 +131,7 @@ export default function
             </div>
           </div>
           
-          {/* <div class="company-activation-email">
-            <span class="company-activation-text04">
-              <span class="company-activation-text05">
-                company Registration id *
-              </span>
-            </span>
-            <input
-              type="text"
-              placeholder="E1398hd"
-              class="company-activation-input1"
-            />
-          </div>
-          <div class="company-activation-pass">
-            <span class="company-activation-text06">
-              <span class="company-activation-text07">Area of Work *</span>
-            </span>
-            <div class="company-activation-input2">
-              <span class="company-activation-text08">
-                <span>rfergrgrgrgg</span>
-              </span>
-              <img
-                src="public/external/vector0308-s88f.svg"
-                alt="Vector0308"
-                class="company-activation-vector"
-              />
-            </div>
-          </div>
-          <div class="company-activation-name1">
-            <span class="company-activation-text10">
-              <span class="company-activation-text11">Industry Type *</span>
-            </span>
-            <input
-              type="text"
-              placeholder="Steven Stallion"
-              class="company-activation-input3"
-            />
-          </div>
-          <div class="company-activation-name2">
-            <span class="company-activation-text12">
-              <span class="company-activation-text13">Registerd office *</span>
-            </span>
-            <input
-              type="text"
-              placeholder="Steven Stallion"
-              class="company-activation-input4"
-            />
-          </div>
-          <div class="company-activation-email1">
-            <span class="company-activation-text14">
-              <span class="company-activation-text15">No of Employees *</span>
-            </span>
-            <input
-              type="text"
-              placeholder="Example@gmail.com"
-              class="company-activation-input5"
-            />
-          </div>
-          <div class="company-activation-name3">
-            <span class="company-activation-text16">
-              <span class="company-activation-text17">Service type *</span>
-            </span>
-            <input
-              type="text"
-              placeholder="Steven Stallion"
-              class="company-activation-input6"
-            />
-          </div>
-          <div class="company-activation-name4">
-            <span class="company-activation-text18">
-              <span class="company-activation-text19">Payment Mode *</span>
-            </span>
-            <input
-              type="text"
-              placeholder="Steven Stallion"
-              class="company-activation-input7"
-            />
-          </div>
-          <div class="company-activation-name5">
-            <span class="company-activation-text24">
-              <span class="company-activation-text25">Work Location *</span>
-            </span>
-            <div class="company-activation-input8">
-              <span class="company-activation-text26">
-                <span>efegregrgreg</span>
-              </span>
-              <img
-                src="public/external/vector0311-bqt.svg"
-                alt="Vector0311"
-                class="company-activation-vector1"
-              />
-            </div>
-          </div> */}
-          
-          
-          
+                  
         </div>
       </div>
 
